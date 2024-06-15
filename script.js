9faede573fb2448552400de2735bd14b4a00da90
Tshembho326 <130842552+Tshembho326@users.noreply.github.com>
2024-06-15 12:57:31 +0200

    Add files via upload
    My first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('revealButton').addEventListener('click', function() {
+    document.getElementById('message').style.display = 'block';
+    this.style.display = 'none';
+});
